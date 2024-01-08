5729c3ae189abb62b84036d58ecd81c5c5436389
vinay kolhe <71378015+vinaykolhe20@users.noreply.github.com>
2024-01-08 04:25:19 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+// singleton
+
+// objects literals
+const JsUser = {
+    name :"Vinay",
+    age:22,
+    location :"Dubai",
+    email:"vinay@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(JsUser.age);
